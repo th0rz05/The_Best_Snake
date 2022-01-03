cdd49edbf23e170b7d9f78e0eac1dd4ef8904000
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-01-03 18:30:53 +0000

    test set position pass

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -16,10 +16,10 @@ public class Position {
     }
 
     public void setX(int x) {
-        this.x = 0;
+        this.x = x;
     }
 
     public void setY(int y) {
-        this.y = 0;
+        this.y = y;
     }
 }
