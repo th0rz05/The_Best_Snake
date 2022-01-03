a03e6062b5c7c42077972b7b0dc4e069c42b0b7e
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-01-03 18:28:10 +0000

    test position

--- src/main/java/Position.java
+++ src/main/java/Position.java
@@ -1,2 +1,18 @@
 public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return 0;
+    }
+
+    public int getY() {
+        return 0;
+    }
+
 }
--- src/test/java/PositionTest.java
+++ src/test/java/PositionTest.java
@@ -1,2 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PositionTest {
+    @Test
+    public void getX(){
+        Position p = new Position(1,2);
+        Assertions.assertEquals(1,p.getX());
+    }
+
+    @Test
+    public void getY(){
+        Position p = new Position(1,2);
+        Assertions.assertEquals(2,p.getY());
+    }
 }
