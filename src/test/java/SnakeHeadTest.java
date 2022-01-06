2322f0a09e0634666d5c2159d1f57cebf20324bb
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-01-06 15:52:31 +0000

    testes snake head

--- src/test/java/SnakeHeadTest.java
+++ src/test/java/SnakeHeadTest.java
@@ -1,2 +1,40 @@
+
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.DefaultTextGUIGraphics;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+
 public class SnakeHeadTest {
+    TerminalScreen screen;
+    SnakeHead snakeHead;
+    @BeforeEach
+    public void setup() throws IOException {
+        TerminalSize terminalSize = new TerminalSize(100, 100);
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+        Terminal terminal = terminalFactory.createTerminal();
+        screen = new TerminalScreen(terminal);
+        snakeHead = new SnakeHead(new Position(10,10));
+    }
+
+    @Test
+    public void test_draw(){
+        snakeHead.draw(screen.newTextGraphics());
+        Assertions.assertEquals(snakeHead.getSymbol(),screen.getBackCharacter(10,10).getCharacterString());
+    }
+
+    @Test
+    public void test_draw2(){
+
+        snakeHead.draw(screen.newTextGraphics());
+        Assertions.assertNotEquals(snakeHead.getSymbol(),screen.getBackCharacter(20,20).getCharacterString());
+    }
 }
