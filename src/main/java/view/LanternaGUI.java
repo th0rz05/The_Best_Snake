35394865a9c68976e111b8ec8cee01f8298e70f2
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-01-07 14:59:38 +0000

    class lanterna

--- src/main/java/view/LanternaGUI.java
+++ src/main/java/view/LanternaGUI.java
@@ -1,4 +1,64 @@
 package view;
 
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class LanternaGUI {
+    TerminalScreen screen;
+    TextGraphics graphics;
+    int height;
+    int width;
+
+    public LanternaGUI(int h, int w){
+        this.height = h;
+        this.width = w;
+        try{
+            TerminalSize terminalSize = new TerminalSize(height, width);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            this.screen = new TerminalScreen(terminal);
+            this.graphics = screen.newTextGraphics();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public TerminalScreen getScreen() {
+        return screen;
+    }
+
+    public void setScreen(TerminalScreen screen) {
+        this.screen = screen;
+    }
+
+    public TextGraphics getGraphics() {
+        return graphics;
+    }
+
+    public void setGraphics(TextGraphics graphics) {
+        this.graphics = graphics;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
 }
