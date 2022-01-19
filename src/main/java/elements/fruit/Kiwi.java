6cb694e121cadbc6e1d00e445d1e9ea9efa527e8
th0rz05 <73318820+th0rz05@users.noreply.github.com>
2022-01-19 09:57:20 +0000

    minor fixes

--- src/main/java/elements/fruit/Kiwi.java
+++ src/main/java/elements/fruit/Kiwi.java
@@ -6,7 +6,7 @@ public class Kiwi extends Fruit {
     public Kiwi(Position position) {
         super(position);
         setSymbol("k");
-        setColor("#00ff00");
+        setColor("#046F3A");
         setVelocity(2);
         setSize(-5);
     }
